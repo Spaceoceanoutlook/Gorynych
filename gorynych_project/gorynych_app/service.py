a7c1a8bbfc6e0f8e08db10de01c7904b5994e49c
Spaceoceanoutlook <spaceocean@outlook.com>
2023-10-18 14:36:28 +0500

    add algorythm check comp

--- gorynych_project/gorynych_app/service.py
+++ gorynych_project/gorynych_app/service.py
@@ -7,7 +7,7 @@ consonant_letters = ["Б", "В", "Г", "Д", "Ж", "З", "Й",
 vowel_letters = ["А", "Е", "Ё", "И", "О", "У", "Ы", "Э", "Ю", "Я"]
 choosing_a_number = [0, 1, 2, 3]
 
-deck = random.sample(consonant_letters, 6) + random.sample(vowel_letters, 4)
+deck = random.sample(consonant_letters, 8) + random.sample(vowel_letters, 5)
 number = random.choice(choosing_a_number)
 
 
@@ -18,10 +18,13 @@ def comp_words():
 class Words:
     players_word_list = []
     comp_word_list = []
+    final_comp_word_list = []
+    temp = 0
 
-    def __init__(self, d, n):
+    def __init__(self, d: list, n: int):
         self.deck = d
-        self.number = n
+        self.number_user = n
+        self.number_comp = n
 
     def checking_for_all_letters(self, w: str):
         dict_of_letters = {}
@@ -34,14 +37,14 @@ class Words:
                 dict_of_letters[i] += 1
         sum_letters = sum(dict_of_letters.values())
         count_letters = len(dict_of_letters)
-        if self.number == 0 and sum_letters > count_letters:
+        if self.number_user == 0 and sum_letters > count_letters:
             return 'Горыныч без голов'
-        if self.number < sum_letters - count_letters:
+        if self.number_user < sum_letters - count_letters:
             return 'Ваше слово превышает количество голов Горыныча'
         else:
-            self.number -= sum_letters - count_letters
-        Words.players_word_list.append(w)
-        return 'Слово прошло проверку'
+            self.number_user -= sum_letters - count_letters
+            self.temp = sum_letters - count_letters
+        self.players_word_list.append(w)
 
     def words_of_comp(self):
         for word in comp_words():
@@ -51,5 +54,26 @@ class Words:
             else:
                 self.comp_word_list.append(word.upper())
 
+    def check_words_of_comp(self):
+        for word in self.comp_word_list:
+            dict_of_letters_comp = {}
+            for letter in word:
+                if letter not in dict_of_letters_comp:
+                    dict_of_letters_comp[letter] = 1
+                else:
+                    dict_of_letters_comp[letter] += 1
+            print(dict_of_letters_comp)
+            sum_letters = sum(dict_of_letters_comp.values())
+            count_letters = len(dict_of_letters_comp)
+            if self.number_comp == 0 and sum_letters > count_letters:
+                continue
+            if self.number_comp < sum_letters - count_letters:
+                continue
+            else:
+                self.number_comp -= sum_letters - count_letters
+            print(self.number_comp)
+            self.final_comp_word_list.append(word)
+
 
 game = Words(deck, number)
+
--- gorynych_project/gorynych_app/views.py
+++ gorynych_project/gorynych_app/views.py
@@ -4,4 +4,22 @@ from .service import game
 
 def index(request):
     context = {'game': game}
+    if request.method == 'POST' and 'add' in request.POST:
+        word = request.POST.get('word').upper()
+        res = game.checking_for_all_letters(word)
+        new_context = {'res': res} | context
+        return render(request, 'gorynych_app/index.html', context=new_context)
+    if request.method == 'POST' and 'cancel' in request.POST:
+        if len(game.players_word_list) >= 1:
+            game.number_user += game.temp    # Возврат Горыныча при отмене слова
+            game.players_word_list.pop()
+            game.temp = 0
+    if request.method == 'POST' and 'count' in request.POST:
+        res = f'Количество ваших слов: {len(game.players_word_list)}'
+        new_context = {'res': res} | context
+        return render(request, 'gorynych_app/index.html', context=new_context)
+    if request.method == 'POST' and 'check' in request.POST:
+        game.words_of_comp()
+        game.check_words_of_comp()
+        return render(request, 'gorynych_app/final.html', context=context)
     return render(request, 'gorynych_app/index.html', context=context)
